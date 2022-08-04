17c56b0152cecca6600eef8ae5cdb6ca1ce80c5c
Chura <churabrady@gmail.com>
2022-08-04 10:01:19 -0700

    Task Manager API

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -5,8 +5,12 @@ const getAllTasks = (req,res)=>{
   res.send('all items from the file')
 }
 const createTask = async(req,res)=>{
-  task = await Task.create(req.body)
-  res.status(201).json({task})
+  try{
+    const task = await Task.create(req.body)
+    res.status(201).json({task})
+  }catch (error){
+    res.status(500).json({msg:error})
+  }
 }
 const getTask = (req,res)=>{
   res.json({id:req.params.id})
